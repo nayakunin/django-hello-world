08d0626d3644e64496f5600a30b31706c91dc924
Nikita Iakunin <nikita.iakunin@deliveryhero.com>
2023-05-18 12:57:05 +0200

    deploy

--- example/views.py
+++ example/views.py
@@ -1,16 +1,11 @@
 # example/views.py
 from datetime import datetime
 
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    context = {
+        'now': now,
+    }
+    return render(request, 'index.html', context)
