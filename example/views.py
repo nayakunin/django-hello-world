9db98b55032c6f990589fa73d39efbdfd4c69fa7
Nikita Iakunin <nikita.iakunin@deliveryhero.com>
2023-05-19 16:12:49 +0200

    commit

--- example/views.py
+++ example/views.py
@@ -4,8 +4,22 @@ from datetime import datetime
 from django.shortcuts import render
 
 def index(request):
-    now = datetime.now()
-    context = {
-        'now': now,
-    }
-    return render(request, 'index.html', context)
+    print(request.method)
+    if request.method == 'POST':
+        result = {
+            "url": request.POST['url'],
+            "values": [{
+                "key": "date",
+                "value": "2021-10-10"
+            }, {
+                "key": "date",
+                "value": "2021-10-10"
+            }, {
+                "key": "date",
+                "value": "2021-10-10"
+            }]
+        }
+
+        return render(request, 'index.html', {'result': result})
+    
+    return render(request, 'index.html', { 'result': '' })
